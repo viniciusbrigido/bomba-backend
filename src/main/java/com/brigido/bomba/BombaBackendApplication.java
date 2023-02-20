0ce627e7f8af2d200301d2e0d82a3876a01ff396
Vinicius Brigido <brigido.viniciuss@gmail.com>
2023-02-20 16:03:23 -0300

    Iniciado projeto

---
+++ src/main/java/com/brigido/bomba/BombaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.brigido.bomba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BombaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BombaBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/brigido/bomba/BombaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brigido.bomba;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BombaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
